df85bf10cc29aafd2afc4a6d58174fe96083f834
Bgst <adit34072@gmail.com>
2024-02-09 01:26:48 +0700

    update

---
+++ php/Konek.php
@@ -0,0 +1,15 @@
+<?php
+//Kalo pake laptop ubah 127.0.0.1 ke localhost
+ $servername = "127.0 0.1";
+ $dbname = "data_user";
+ $username = "root";
+ $pass = "root";
+ 
+ $conn = new mysqli_connect($servername, $dbname, $username, $pass);
+ 
+ if (!$conn) {
+  die("Koneksi Gagal: " . mysqli_error());
+ } else {
+   echo "Koneksi Berhaail";
+ }
+?>
